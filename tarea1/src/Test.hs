f94acf13d0050937f87cfeee1ed76601e5cf6fef
gabodornelas <gabodornelas@gmail.com>
2025-10-16 17:51:26 -0400

    Listo

--- tarea1/src/Test.hs
+++ tarea1/src/Test.hs
@@ -3,7 +3,7 @@
 -- Usa el framework HUnit. Para compilar y correr:
 -- 1. Asegúrate de tener la librería HUnit instalada (e.g., usando cabal o stack).
 -- 2. Compila: ghc --make Test.hs
--- 3. Ejecuta: ./Test.exe
+-- 3. Ejecuta: ./Test.exe (windows)
 -------------------------------------------------------------------------------
 
 import Tarea1
